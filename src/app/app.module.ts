3bb1793df2d0cafd6a41ee097c5d8d59396e3d02
Marbelis Poleo <poleomoncada@gmail.com>
2023-01-07 01:19:31 -0300

    God of war

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    SliderComponent,
+    ToolbarComponent,
+    IntroComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
